1b04a15b07905a8b2c77476f1dd5100395ecd864
CheeseBoyd <ajlanot@gmail.com>
2017-06-21 16:09:59 +0800

    js obj iteration tests

--- Regex Tests/script.js
+++ Regex Tests/script.js
@@ -1,10 +1,28 @@
+/*
+* Testing ground
+*/
+
 document.getElementById("status").innerHTML = "A regex that outputs all the matches found in a string given the match item";
 
 var inputString = "Mick gordeon's rip and tear. Soundtrack";
 var matchkeywords = "rip and tear";
-var matchKeywords = matchkeywords.toLowerCase();
-var outputString = "";
-var regex = /rip/ + /matchkeywords/;
+var regex = /\s/gi;
+
+
+// will split text into tokens based off the regular expression
+
+function tokenizer(message, regex) {
+	var tokenizer = message.split(regex);
+	return tokenizer; 
+}
+
+
+var outputString = tokenizer(inputString, regex );
+
+var concatString = '';
+for(var i = 0; outputString.length > i; i++) {
+
+}
 
 if(outputString || regex || inputString || matchkeywords || inputString) {
 document.getElementById("regex").innerHTML = "Regex: " + regex;
@@ -15,13 +33,98 @@ document.getElementById("output-string").innerHTML = "output-string: " + outputS
 } else {
 	console.log("error");
 }
-// will split text into tokens based off the regular expression
 
-function tokenizer(message, regex) {
-	var tokenizer = message.split(regex);
-	return tokenizer; 
+
+function mockUser(messageText) {
+	let tokenizer = messageText.split(/\s/gi);
+	let mockString = "";
+	for(let word of tokenizer) {
+		word.split
+		console.log(word);
+	}
+
+}
+
+mockUser("Hello World");
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let a = "aaron";
+
+let val = getRandomIntInclusive(0,a.length);
+
+let aChar = a.charAt(val).toUpperCase();
+
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, (txt)=>{
+    	return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+var fun = (messageText)=> {
+          let tokenizer = messageText.split(/\s/gi);
+          let out = tokenizer.join("");
+          return out;
+        }
+
+console.log(fun("hola negra"));
+
+let speech = {
+  greet: ["hello", "hi", "hola"],
+  goodbye:  ["bye", "goodbye", "good bye", "good-bye", "bye-bye","bye bye"],
+  inquire: ["what", "?", "help","do something"]
+}
+var sp = Object.keys(speech);
+// console.log(sp);
+
+console.log("Break-----------Break");
+
+sp.forEach(function(item){
+  if(speech[item][0]) {
+   //  console.log(speech[item][0]);
+  }
+
+});
+
+/*
+
+var keys = Object.keys(speech);
+var count = 0;
+for(var object of keys){
+  console.log(speech[object].length);
+  if(speech[object][count]) {
+    console.log(speech[object]);
+  }
+  count++;
+  console.log("break");
+}
+
+*/
+
+var keys = Object.keys(speech);
+
+for(var object of keys){
+  // len of each speech array
+  console.log(speech[object].length);
+
+  // output every item in each array
+  for(var ob = 0;ob < speech[object].length; ob++) {
+    console.log(speech[object][ob]);
+  }
 }
 
 
-var arr = tokenizer("still feel like your man", /\s+/ );
-console.log(arr);
+
+/*
+for(var ob in speech) {
+  console.log(ob);
+  for(var ob2 in ob) {
+    console.log(ob2);
+  }
+}
+*/
